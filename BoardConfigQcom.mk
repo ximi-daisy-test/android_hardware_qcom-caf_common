fc677ce7e4d06af9c81a724e7167fc4d670078d9
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-01-30 17:52:51 +0800

    Reapply "qcom: Include additional wlan soong namespace"
    This reverts commit dfa40b066e94761a6e38762c4c721ec7e045da7c.

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -463,7 +463,9 @@ else
 endif
 
 # Add wlan to PRODUCT_SOONG_NAMESPACES
-PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/wlan
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/qcom-caf/wlan \
+    hardware/qcom-caf/wlan/qcwcn
 
 # Verified Boot
 BOARD_AVB_SYSTEM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
